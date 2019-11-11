4de9594b5718b14b908d1494eee48841528a1eac
mojo46 <manojmj96@hotmail.com>
2019-11-11 19:15:24 +0530

    change

--- train_test.py
+++ train_test.py
@@ -1,8 +1,8 @@
 import glob, os
 # Current directory
-current_dir = os.path.dirname(os.path.abspath(__file__))
+current_dir = os.getcwd()
 print(current_dir)
-current_dir = 'quality_check'
+current_dir = 'RenaultKwidImages'
 # Percentage of images to be used for the test set
 percentage_test = 10;
 # Create and/or truncate train.txt and test.txt
@@ -11,7 +11,8 @@ file_test = open('test.txt', 'w')
 # Populate train.txt and test.txt
 counter = 1  
 index_test = round(100 / percentage_test)  
-for pathAndFilename in glob.glob(os.path.join(current_dir, "*.jpg")):  
+for pathAndFilename in glob.glob(os.path.join(current_dir, "*.jpg")):
+	print("hello")  
 	title, ext = os.path.splitext(os.path.basename(pathAndFilename))
 	if counter == index_test:
 		counter = 1
