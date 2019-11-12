8614a5fa15804076b6dba0b757f5ac1e9e698f05
mojo46 <manojmj96@hotmail.com>
2019-11-12 17:14:49 +0530

    del

--- train_test.py
+++ train_test.py
@@ -1,24 +1,23 @@
-
 from imutils import paths
+import os
 current_dir = 'RenaultKwidImages'
 print("[INFO] loading images...")
 imagePaths = list(paths.list_images(current_dir))
-print(imagePaths)
-
-# Percentage of images to be used for the test set
-percentage_test = 10;
-# Create and/or truncate train.txt and test.txt
 file_train = open('train.txt', 'w')  
 file_test = open('test.txt', 'w')
-# Populate train.txt and test.txt
+percentage_test = 10
+
 counter = 1  
 index_test = round(100 / percentage_test)  
-for imagepath in imagePaths:
-	print("hello")
 
+
+for image in imagePaths:
+	path = os.path.dirname(image)+'/'+os.path.basename(image)
+	file_train.write(path+'\n')
 	if counter == index_test:
 		counter = 1
-		file_test.write(imagepath +"\n")
+		print(path)
+		file_test.write(path + "\n")
 	else:
-		file_train.write(imagepath +"\n")
+		file_train.write(path + "\n")
 		counter = counter + 1
